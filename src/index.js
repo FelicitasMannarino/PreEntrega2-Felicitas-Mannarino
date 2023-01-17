2c58d4f3a5a5c330950815154b58f922cdfa0eba
felicitas mannarino <felimannarino@gmail.com>
2023-01-17 09:21:27 -0300

    primer commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import NavBar from './components/navBar/NavBar.js';
+import ItemListContainer from './components/itemListContainer/ItemListContainer.js';
+import Footer from './components/footer/Footer.js';
+
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <NavBar/>
+    <ItemListContainer/>
+    <Footer/>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
